610a88a73c724acbe568fac5b90cb9d4d1924477
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-11 15:57:09 +0100

    Adding filtering and sorting to books on admin page

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -4,5 +4,7 @@ from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("rating", "author",)
+    list_display = ("title", "author")
 
 admin.site.register(Book, BookAdmin)
