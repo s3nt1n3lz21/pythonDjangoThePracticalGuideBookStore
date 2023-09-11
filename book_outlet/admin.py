9837f5432de550ac106d8906a3c070dd13a86c1b
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-11 15:55:06 +0100

    Enabled editing of books on admin page

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Book, BookAdmin)
