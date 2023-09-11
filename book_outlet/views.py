9bca387d674e14befad9d9ece863c4acbbd0f180
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-11 15:23:00 +0100

    Added slugs and used aggregations

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,19 +1,25 @@
 # from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 from .models import Book
+from django.db.models import Avg, Min
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    aggregations = books.aggregate(Avg("rating"), Min("rating"))
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": aggregations["rating__avg"],
+        "min_rating": aggregations["rating__min"]
     })
 
-def book_detail(request, id):
+def book_detail(request, slug):
     # try:
     #     book = Book.objects.get(pk=id)
     # except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk=id)
+    book = get_object_or_404(Book, slug=slug)
     return render(request, "book_outlet/book_detail.html", {
         "title": book.title,
         "author": book.author,
