56cc2289ddad1f1f9d271c42ce1d424fedaa78ea
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2023-09-12 13:49:59 +0100

    Added author model to admin page

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Author, Book
 
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("rating", "author",)
     list_display = ("title", "author")
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_filter = ("last_name",)
+    list_display = ("first_name", "last_name")
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
